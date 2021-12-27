b15fc015fe2e7e91ee080b00436806d2fbe1f4bb
Matheus Jacques <matheus.jacques@live.com>
2021-12-27 23:54:51 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,8 +3,8 @@
 	if (isset($_POST['email'])) {
 
 		// EDIT THE 2 LINES BELOW AS REQUIRED
-		$email_to = "hey@sarozpoddar.com.np";
-		$email_subject = "My offer for [Your Domain]";
+		$email_to = "jacquesmats@gmail.com";
+		$email_subject = "My offer for your domain";
 
 
 		$name = $_POST['name']; // required
