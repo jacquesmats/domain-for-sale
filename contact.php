b54f00b5c4088fb62a4019bda9ed84ee799afe0d
Matheus Jacques <matheus.jacques@live.com>
2021-12-28 00:43:22 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@
 	if (isset($_POST['email'])) {
 
 		// EDIT THE 2 LINES BELOW AS REQUIRED
-		$email_to = "jacquesmats@gmail.com";
+		$email_to = "maybemark@protonmail.com";
 		$email_subject = "My offer for your domain";
 
 
